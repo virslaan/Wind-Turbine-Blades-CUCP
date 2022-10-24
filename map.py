eea942aa399a31395aabeac8ee3b3266f9de7fa1
Jiayuan Cui <90983614+JiayuanCui@users.noreply.github.com>
2022-10-24 00:34:27 -0400

    upload map.py

--- map.py
+++ map.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import seaborn as sns
 from streamlit_folium import st_folium
 import folium
